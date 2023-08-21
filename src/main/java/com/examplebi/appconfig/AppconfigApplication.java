63e43c3fe3b6ee226c3749a22ef10ba97751d3b0
bijunjian <bijunjian@thunisoft.com>
2023-08-21 23:41:10 +0800

    第一次

---
+++ src/main/java/com/examplebi/appconfig/AppconfigApplication.java
@@ -0,0 +1,13 @@
+package com.examplebi.appconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppconfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/examplebi/appconfig/AppconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplebi.appconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppconfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
